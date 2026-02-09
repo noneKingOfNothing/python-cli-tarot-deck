159493de66625154966a75f074be9f0a502ccf9d
noneKingOfNothing <jethompson2025@gmail.com>
2026-02-09 18:32:01 -0500

    Update python-cli-tarot-deck.py for readability
    edited for readability - lines were stuck together

--- python-cli-tarot-deck.py
+++ python-cli-tarot-deck.py
@@ -1,4 +1,5 @@
 #    Python CLI Tarot Deck
+
 #    Draws a given number of tarot cards from the deck and present them to the user.
 
 #    credit 'astrology.com/article/order-of-tarot-cards' for the tarot card list
@@ -8,6 +9,7 @@ import random , linecache
 
 '''make a function that uses a random number to reference one of the lines
 in a list of card names, use a list to make sure you don't pull two of the same cards'''
+
 def draw_cards(number_to_draw):
     for x in range(0 , int(number_to_draw)):
         drawn_card_tracker = []
@@ -20,7 +22,7 @@ def draw_cards(number_to_draw):
         print(line)
 
 # ask for the number of cards to draw and fire the function to pull the right number:
-number_to_draw = input("How many cards? Use a numeral.")
+number_to_draw = input("How many cards? Please use a numeral.")
 draw_cards(number_to_draw)
 
 
