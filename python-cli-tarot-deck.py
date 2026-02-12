91f2896cc053025f4cf35b53bb7d294fd9b3e239
Jack <jethompson2025@gmail.com>
2026-02-11 21:33:56 -0500

    Added LLM integration for interpretations

--- python-cli-tarot-deck.py
+++ python-cli-tarot-deck.py
@@ -4,8 +4,9 @@
 
 #    credit 'astrology.com/article/order-of-tarot-cards' for the tarot card list
 
-# grab the library to generate random numbers and linecache to pull the proper line
+#we need random numbers, line references, system tools (to exit), and ollama's chat to interact with LLMs'
 import random , linecache , sys
+from ollama import generate
 
 #make a variable outside the functions to hold drawn cards
 drawn_card_tracker = []
@@ -19,19 +20,36 @@ def draw_cards(number_to_draw):
         line = linecache.getline('./cardnames.txt', i)
         line = line.rstrip() # this removes the trailing /n
         drawn_card_tracker.append(line)
+        print()
         print(line)
 
 
 # ask for the number of cards to draw and fire the function to pull the right number:
-number_to_draw = input("How many cards? Please use a numeral.")
+print(2 * '\n')
+number_to_draw = input("How many cards would you like to draw from the deck? Please use a numeral.    ")
+
+#add space to make things readable
+print(2 * '\n')
+
 # fire the function to draw cards
 draw_cards(number_to_draw)
 
-# ask if the user wants an interpretation. If no: exit, if yes: turn the card list into a string and send the string to an llm with ollama
-yes_or_no = input("Would you like an interpretation?")
 
-if yes_or_no == 'no':
+# ask if the user wants an interpretation of the draw. If no: exit, if yes: turn the card list into a string and send the string to an llm with ollama.
+print(2 * '\n')
+yes_or_no = input("Would you like an interpretation?  ")
+
+if yes_or_no == 'yes':
+   drawn_card_tracker = ', '.join(drawn_card_tracker)
+   response = generate(
+       model='dolphin-llama3:8b',
+       prompt=f'You are a professional tarot card reader. Please tell me what symbols appear in the following cards and what the cards mean. They are {drawn_card_tracker}'
+       )
+   print(2 * '\n')
+   print(response.response)
+   print(2 * '\n')
+
+else:
     sys.exit()
-#else:
 
     
